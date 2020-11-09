1abfd31bd4b4395f089f033d7533d6603a3ddbce
Florian Lepage <Chigusa.Kayano@outlook.com>
2020-11-09 12:38:07 +0100

    Create Database.js
    base code added
    discord.js v12.4.1 added

---
+++ src/database/Database.js
@@ -0,0 +1,48 @@
+//Database.js// -- created by Florian Lepage 09/11/2020
+
+// mysql2 lib import
+const mysql = require("mysql2");
+
+class Database {
+    constructor(token, time=null) {
+        this.dbUsername = token.database.username;
+        this.dbDatabase = token.database.database;
+        this.dbPassword = token.database.password;
+        this.dbHost = token.database.host;
+        this.dbPort = token.database.port;
+        this.time = time;
+    }
+    connection() { // connection method for mysql queries
+        return mysql.createConnection({
+            user: this.dbUsername,
+            database: this.dbDatabase,
+            password: this.dbPassword,
+            host: this.dbHost,
+            port: this.dbPort
+        });
+    }
+    checkConnectionState() { // method to check if the mysql connection state is efficient
+        this.connection().query(`CREATE TABLE IF NOT EXISTS connectionState (state VARCHAR(30))`, (err) => {
+            if(err) {
+                throw err;
+            } else {
+                return console.log(`[${this.time(Date.now())}] The connection between the client & the Database has been established.`);
+            }
+        });
+    }
+    tableCheckCreation() { // methode to create all the required mysql tables
+        this.connection().query(`CREATE TABLE IF NOT EXISTS dc_staff_members (user_id VARCHAR(30), added_at VARCHAR(30), added_by VARCHAR(30))`, (err) => {
+            if(err) throw err;
+        });
+        this.connection().query(`CREATE TABLE IF NOT EXISTS dc_private_channels (channel_id VARCHAR(30), guild_id VARCHAR(30), role_id VARCHAR(30), renewal_id VARCHAR(255), added_at VARCHAR(30), added_by VARCHAR(30))`, (err) => {
+            if(err) throw err;
+        });
+        this.connection().query(`CREATE TABLE IF NOT EXISTS dc_purchased_users (user_id VARCHAR(30), order_id VARCHAR(30), time VARCHAR(30), channel_id VARCHAR(30))`, (err) => {
+            if(err) throw err;
+        });
+    }
+}
+
+module.exports = {
+    Database
+}
---
+++ src/index.js
@@ -0,0 +1,3 @@
+//index.js// -- created by Florian Lepage 11/09/2020
+
+const discord = require("discord.js");
