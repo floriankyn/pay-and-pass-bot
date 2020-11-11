4b1eb51ddb95c5b44dc56275ec69d274bd19a8f4
Florian Lepage <Chigusa.Kayano@outlook.com>
2020-11-11 04:23:07 +0100

    Update index.js
    Add webapp gateway

--- src/index.js
+++ src/index.js
@@ -19,6 +19,7 @@ const {Database} = require("./database/Database.js");
 const {StaffManager} = require("./action/administration/StaffManager.js");
 const {Utils} = require("./action/everyone/Utils.js");
 const {PrivateManager} = require("./action/moderation/PrivateManager.js");
+const {PrivateStoreManager} = require("./action/everyone/PrivateStoreManager.js");
 
 // Event imports
 
@@ -34,7 +35,57 @@ client.on(`message`, (message) => {
     new StaffManager(message, config, language, client).selector();
     new Utils(message, config, language, client).selector();
     new PrivateManager(message, config, language, client).selector();
+    new PrivateStoreManager(message, config, language, client).selector();
 });
 
 // Login to Discord Api
 client.login(config.discord.token).then().catch(console.error);
+
+// web gateway
+const express = require('express')
+const bodyparser = require('body-parser')
+const path = require('path')
+const app = express()
+
+const Publishable_Key = config.stripe.publishableKey;
+const Secret_Key = config.stripe.secretKey;
+
+const stripe = require('stripe')(Secret_Key)
+
+const port = process.env.PORT || 4000
+
+app.use(bodyparser.urlencoded({extended:false}))
+app.use(bodyparser.json())
+app.use(express.static(__dirname+"/views/"));
+
+app.get('/', function(req, res){
+    res.sendFile(path.join(__dirname+'/views/index.html'));
+})
+
+app.post('/payment', function(req, res){
+    // Moreover you can take more details from user
+    // like Address, Name, etc from form
+    stripe.customers.create({
+        email: req.body.stripeEmail,
+        source: req.body.stripeToken,
+    })
+        .then((customer) => {
+            return stripe.charges.create({
+                amount: 7000,    // Charing Rs 25
+                description: 'Web Development Product',
+                currency: 'USD',
+                customer: customer.id
+            });
+        })
+        .then((charge) => {
+            res.send("Success") // If no error occurs
+        })
+        .catch((err) => {
+            res.send(err)    // If some error occurs
+        });
+})
+
+app.listen(port, function(error){
+    if(error) throw error
+    console.log(`[${time(Date.now())}] Web app Listening on port: ${port}`)
+})
