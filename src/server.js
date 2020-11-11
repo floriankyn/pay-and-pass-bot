e5c1d49d664719a3cc096cc2658dca814a9b503f
Florian Lepage <Chigusa.Kayano@outlook.com>
2020-11-11 22:40:24 +0100

    Update server.js
    Server routes updated each 10000 secs

--- src/server.js
+++ src/server.js
@@ -3,11 +3,12 @@
 // express init
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = 3001;
 
 // pug render engine set
 const pug = require("pug")
 app.set("view engine", "pug");
+app.use(express.static(__dirname + "/views"))
 
 // config import
 const fs = require("fs");
@@ -31,28 +32,30 @@ app.get('/', (req, res) => {
 });
 
 // product route creation
-stripe.products.list({
-    active: true
-}).then(respond => {
-   for(let i = 0; respond.data.length > i; i++) {
-       app.get(`/${respond.data[i].id}/`, (req, res) => {
-           db.connection().query(`SELECT * FROM dc_private_channels WHERE stripe_product_id = "${respond.data[i].id}"`, async (err, rows) => {
-               await stripe.paymentIntents.create({
-                   amount: rows[0].price * 100,
-                   currency: "usd",
-                   description: "Access to the private channel."
-               }).then(respond => {
-                   let channel = client.channels.cache.get(rows[0].channel_id);
-                   res.render('product', {
-                       price: parseInt(respond.amount) / 100,
-                       channelName: `#${channel.name}`,
-                       channelDescription: `${channel.topic !== null ? channel.topic : `Private access to #${channel.name} for $${parseInt(respond.amount) / 100} per month.`} `,
-                   });
-               }).catch(console.error);
+setInterval(() => {
+    stripe.products.list({
+        active: true
+    }).then(respond => {
+       for(let i = 0; respond.data.length > i; i++) {
+           app.get(`/${respond.data[i].id}/`, (req, res) => {
+               db.connection().query(`SELECT * FROM dc_private_channels WHERE stripe_product_id = "${respond.data[i].id}"`, async (err, rows) => {
+                   await stripe.paymentIntents.create({
+                       amount: rows[0].price * 100,
+                       currency: "usd",
+                       description: "Access to the private channel."
+                   }).then(respond => {
+                       let channel = client.channels.cache.get(rows[0].channel_id);
+                       res.render('product', {
+                           price: parseInt(respond.amount) / 100,
+                           channelName: `${channel.name}`,
+                           channelDescription: `${channel.topic !== null ? channel.topic : `Private access to #${channel.name} for $${parseInt(respond.amount) / 100} per month.`} `,
+                       });
+                   }).catch(console.error);
+               });
            });
-       });
-   }
-}).catch(console.error);
+       }
+    }).catch(console.error);
+}, 10000)
 
 
 app.listen(port, () => {
